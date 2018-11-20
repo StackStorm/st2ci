0e4207bcb34a4c054ec50df82646da4f135af79d
blag <blag@users.noreply.github.com>
2018-11-20 12:12:03 -0700

    Upgrade to Node.js v10 at ST2 v3.0

--- actions/st2_pkg_upgrade_deps_u16.sh
+++ actions/st2_pkg_upgrade_deps_u16.sh
@@ -5,6 +5,12 @@ VERSION=$1
 ENTERPRISE=$2
 SHORT_VERSION=`echo ${VERSION} | cut -d "." -f1-2`
 
+if [ ${SHORT_VERSION} = "3.0" ] && [ ${ENTERPRISE} -eq 0 ]; then
+    echo "Upgrading dependencies for 3.0 community"
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    exit 0
+fi
+
 if [ ${SHORT_VERSION} = "2.4" ] && [ ${ENTERPRISE} -eq 0 ]; then
     echo "Upgrading dependencies for 2.4 community"
     curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
