c4ad9987366057fcdb2af7f7a12115dc9c0cdf5d
blag <blag@users.noreply.github.com>
2021-03-04 02:14:07 -0800

    Remove enterprise options from upgrade actions/scripts

--- actions/st2_pkg_upgrade_deps_el7.sh
+++ actions/st2_pkg_upgrade_deps_el7.sh
@@ -2,10 +2,9 @@
 set -e
 
 VERSION=$1
-ENTERPRISE=$2
 SHORT_VERSION=`echo ${VERSION} | cut -d "." -f1-2`
 
-if [ ${SHORT_VERSION} = "2.10" ] && [ ${ENTERPRISE} -eq 0 ]; then
+if [ ${SHORT_VERSION} = "2.10" ]; then
     echo "Upgrading Node.js repositories"
     sudo sed -i.bak 's|^baseurl=\(https://rpm.nodesource.com\)/[^/]\{1,\}/\(.*\)$|baseurl=\1/pub_10.x/\2|g' /etc/yum.repos.d/nodesource-*.repo
     echo "Upgrading dependencies for 2.10 community"
@@ -15,19 +14,11 @@ if [ ${SHORT_VERSION} = "2.10" ] && [ ${ENTERPRISE} -eq 0 ]; then
     exit 0
 fi
 
-if [ ${SHORT_VERSION} = "2.4" ] && [ ${ENTERPRISE} -eq 0 ]; then
+if [ ${SHORT_VERSION} = "2.4" ]; then
     echo "Upgrading dependencies for 2.4 community"
     curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
     sudo yum clean all
     sudo rpm -e --nodeps npm || true
     sudo yum update -y nodejs
-    exit 0
-fi
-
-if [ ${SHORT_VERSION} = "2.4" ] && [ ${ENTERPRISE} -eq 1 ]; then
-    echo "Upgrading dependencies for 2.4 enterprise"
-    sudo /opt/stackstorm/st2/bin/pip install \
-        --find-links /opt/stackstorm/share/wheels --no-index --quiet \
-        --upgrade st2-enterprise-auth-backend-ldap
     exit 0
 fi
