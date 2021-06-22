963617e49108045315ba297f86696e5c86388883
amanda <amanda.mcguinness@ammeonsolutions.com>
2021-06-22 17:32:27 +0100

    Initial changes for 3.5 release

--- actions/st2_pkg_upgrade_deps_u18.sh
+++ actions/st2_pkg_upgrade_deps_u18.sh
@@ -4,6 +4,13 @@ set -e
 VERSION=$1
 SHORT_VERSION=`echo ${VERSION} | cut -d "." -f1-2`
 
+if [ ${SHORT_VERSION} = "3.5" ]; then
+    echo "Upgrading dependencies for 3.5 community"
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    apt-get install --only-upgrade nodejs
+    exit 0
+fi
+
 if [ ${SHORT_VERSION} = "2.10" ]; then
     echo "Upgrading dependencies for 2.10 community"
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
